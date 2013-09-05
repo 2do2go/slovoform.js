5b0221c73efb6060a14bf64bfaff8b6e1dba22f8
solid <solid@solid-PC.(none)>
2013-09-05 15:58:59 +0400

    New functional was added: now pluralize can take not just words, but whole sentences. Its UNBELIEVABLE!!!

--- test/slovoform.js
+++ test/slovoform.js
@@ -5,15 +5,27 @@ var assert = require('assert'),
 	Genderify = require('../lib/slovoform').Genderify;
 
 var pluralize = new Pluralize(),
-	form1 = 'час', form2 = 'часа', form3 = 'часов';
+	forms = [
+		['событие', 'события', 'событий'],
+		['была', 'были', 'были'],
+		['принята', 'приняты', 'приняты']
+	];
 
-pluralize.add(form1, form2, form3);
+forms.forEach(function(words) {
+	pluralize.add.apply(pluralize, words);
+});
 
-assert.equal(pluralize.get(form1, 1), form1);
-assert.equal(pluralize.get(form1, 2), form2);
-assert.equal(pluralize.get(form1, 3), form2);
-assert.equal(pluralize.get(form1, 5), form3);
-assert.equal(pluralize.get(form1, 20), form3);
+assert.equal(pluralize.get(forms[0][0], 1), forms[0][0]);
+assert.equal(pluralize.get(forms[0][0], 2), forms[0][1]);
+assert.equal(pluralize.get(forms[0][0], 3), forms[0][1]);
+assert.equal(pluralize.get(forms[0][0], 5), forms[0][2]);
+assert.equal(pluralize.get(forms[0][0], 20), forms[0][2]);
+var str = [forms[0][0], forms[1][0], forms[2][0]].join(' ');
+var resStr1 = [forms[0][1], forms[1][1], forms[2][1]].join(' '),
+	resStr2 = [forms[0][2], forms[1][1], forms[2][1]].join(' ');
+assert.equal(pluralize.get(str, 2), resStr1);
+console.log(pluralize.get(str, 5));
+assert.equal(pluralize.get(str, 5), resStr2);
 
 var genderify = new Genderify(),
 	form1 = 'сказал', form2 = 'сказала';
